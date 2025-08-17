bc487ddfac5887cb12222f67d96133ee4058143a
Matara Kemari <drkemari@gmail.com>
2025-08-17 11:17:32 +0300

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dog, setDog] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDog(data.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dog} alt="A Random Dog" />;
+}
+
+export default App;
